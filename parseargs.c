b8ea660c05f2b2f717529986a7b1509be1175e27
michaellikaiye <mye1@stuy.edu>
2018-11-23 13:48:28 -0500

    pipe :(

--- parseargs.c
+++ parseargs.c
@@ -4,60 +4,64 @@
 #include "parseargs.h"
 
 char ** parse_argsSemiColon(char * line) {
-  char ** arr = malloc(ARG_MAX * sizeof(char *));
-  int i = 0;
-  while(line) {
-    arr[i] = strsep(&line, ";");
-    i++;
-  }
+	char ** arr = malloc(ARG_MAX * sizeof(char *));
+	int i = 0;
+	while(line) {
+		arr[i] = strsep(&line, ";");
+		i++;
+	}
 
-  return arr;
+	return arr;
 }
 
-char ** parse_args(int * argc, char * line) {
-  char ** arr = malloc(15 * sizeof(char *));
-  int i = 0;
-  clean_str(line);
-  while(line) {
-    arr[i] = strsep(&line, " ");
-    i++;
-  }
-
-  *argc = i + 1;
-  return arr;
+char * hasPipe(char * line) {
+  char * p = strchr(line, '|');
+  return p;
 }
 
-void replace_multi_string(char *str)
-{
-  char *dest = str;  /* Destination to copy to */
-  while (*str != '\0')
-  {
-    while (*str == ' ' && *(str + 1) == ' ')
-      str++;
-    *dest++ = *str++;
-  }
-  *dest = '\0';
+char ** parse_argsSpace(int * argc, char * line) {
+	char ** arr = malloc(100 * sizeof(char *));
+	int i = 0;
+	clean_str(line);
+	while(line) {
+		arr[i] = strsep(&line, " ");
+		i++;
+	}
+
+	*argc = i + 1;
+	return arr;
+}
+
+void replace_multi_string(char *str) {
+	char *dest = str; /* Destination to copy to */
+	while(*str != '\0')
+	{
+		while(*str == ' ' && *(str + 1) == ' ')
+			str++;
+		*dest++ = *str++;
+	}
+	*dest = '\0';
 }
 
 void trim_whitespace(char *str) {
-  char * cpy = (char *) malloc(strlen(str));
-  strcpy(cpy, str);
-  while((unsigned char) *cpy == ' ') {
-    cpy++;
-  }
-  if(!*cpy) {
-    strcpy(str, cpy);
-    return;
-  }
-  char * e = cpy + strlen(cpy) - 1;
-  while(e > cpy && (unsigned char) *e == ' ') {
-    e--;
-  }
-  e[1] = 0;
-  strcpy(str, cpy);
+	char * cpy = (char *) malloc(strlen(str));
+	strcpy(cpy, str);
+	while((unsigned char) *cpy == ' ') {
+		cpy++;
+	}
+	if(!*cpy) {
+		strcpy(str, cpy);
+		return;
+	}
+	char * e = cpy + strlen(cpy) - 1;
+	while(e > cpy && (unsigned char) *e == ' ') {
+		e--;
+	}
+	e[1] = 0;
+	strcpy(str, cpy);
 }
 
 void clean_str(char *str) {
-  replace_multi_string(str);
-  trim_whitespace(str); 
+	replace_multi_string(str);
+	trim_whitespace(str);
 }
--- shell.c
+++ shell.c
@@ -1,9 +1,9 @@
 /*errors
-  when entering spaces where they don't belong, command takes space as input
-  fix parse_args function
-  big error when entering nothing into shell line
-  fix main
-*/
+   when entering spaces where they don't belong, command takes space as input
+   fix parse_args function
+   big error when entering nothing into shell line
+   fix main
+ */
 
 #include <stdio.h>
 #include <unistd.h>
@@ -19,71 +19,78 @@
 #include "parseargs.h"
 #include "io.h"
 
+//colors
+#define NRM  "\x1B[0m"
+#define GRN  "\x1B[32m"
+#define BLU  "\x1B[34m"
+
 // Function where the system command is executed
 void run(char** args, int argc) {
-  // Forking a child
-  int pid = fork();
-  if(pid == -1) {
-    printf("fysh: failed to fork child\n");
-  }
-  else if(pid == 0) {
-    if(!argc) { return; }
-    if(execvp(args[0], args) < 0) {
-      printf("fysh: failed to execute %s\n", args[0]);
-    }
-    exit(0);
-  } else {
-    // waiting for child to terminate
-    wait(NULL);
-    return;
-  }
+	// Forking a child
+	int pid = fork();
+	if(pid == -1) {
+		printf("fysh: failed to fork child\n");
+	}
+	else if(pid == 0) {
+		if(!argc) { return;
+		}
+		if(execvp(args[0], args) < 0) {
+			if(strcmp(args[0], ""))
+				printf("fysh: command '%s' not found\n", args[0]);
+			// else input is nothing so do nothing
+		}
+		exit(0);
+	} else {
+		// waiting for child to terminate
+		wait(NULL);
+		return;
+	}
 }
 
 void printprompt() {
-  char curD[100];
-  getcwd(curD, sizeof(curD));
-  printf("\x1B[34m");
-  printf("%s", curD);
-  printf("$ ");
-  printf("\x1B[0m");
+	char * name = getenv("USER");
+	char curDir[1000];
+	getcwd(curDir, sizeof(curDir));
+	printf("%s%s:%s%s$ %s", GRN, name, BLU, curDir, NRM);
 }
 
 int main() {
-  //execvp(args[0], args);
-  while(1) {
-    unsigned char line[1000];
-    printprompt();
-    //scanf("%[^\n]%*c", line);
-    liveRead(line, 1000);
-    if(!strcmp(line, "")) {
-    
-    } else {
+	//execvp(args[0], args);
+	while(1) {
+		unsigned char line[1000];
+		printprompt();
+		//scanf("%[^\n]%*c", line);
+		liveRead(line, 1000);
 
-      char ** semiColons = parse_argsSemiColon(line);
-      int i = 0;
-      while(semiColons[i]) {
-        int argc;
-        char ** argv = parse_args(&argc, semiColons[i++]);
-        if(strcmp(argv[0], "exit") == 0) {
-          exit(0);
-        }
-        else if(strcmp(argv[0], "cd") == 0) {
-          if(argc - 1 > 2) {
-            printf("fysh: cd takes 1 argument, %d found\n", argc - 1);
-          }
-          int stat = chdir(argv[1]);
-          if(stat == -1) {
-            printf("fysh: cd: %s\n", strerror(errno));
-          }
-        }
-        else {
-          run(argv, argc);
-        }
-      }
-    }
-  }
+		char ** semiColons = parse_argsSemiColon(line);
+		int i = 0;
+		while(semiColons[i]) {
+			if(hasPipe(semiColons[i]) == NULL) {
+				int argc;
+				char ** argv = parse_argsSpace(&argc, semiColons[i]);
+				if(strcmp(argv[0], "exit") == 0) {
+					exit(0);
+				} else if(strcmp(argv[0], "cd") == 0) {
+					if(argc - 1 > 2) {
+						printf("fysh: cd takes 1 argument, %d found\n", argc - 1);
+						//change print!!
+					}
+					int stat = chdir(argv[1]);
+					if(stat == -1) {
+						printf("fysh: cd: %s\n", strerror(errno));
+					}
+				}
+				else {
+					run(argv, argc);
+				}
+			} else {
+				printf("fysh: cannot pipe yet\n");     //do pipe stuff
+			}
+			i++;
+		}
+	}
 
-  //char * str = args[0];
-  //printdir(str);
-  return 0;
+	//char * str = args[0];
+	//printdir(str);
+	return 0;
 }
