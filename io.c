fe6bc5096fbbd0b38c38f7e175332c3da3f45773
ofrank123 <ofrank@stuy.edu>
2018-11-28 19:33:50 -0500

    pushing begginings of tab completion

--- io.c
+++ io.c
@@ -5,12 +5,17 @@
 #include <fcntl.h>
 #include <stdlib.h>
 #include <string.h>
+#include <dirent.h>
+#include <sys/types.h>
+
+#include "io.h"
 
 #define cursorforward(x) if(x > 0) printf("\033[%dC", (x))
 #define cursorbackward(x) if(x > 0) printf("\033[%dD", (x))
 #define KEY_DEL 127
 #define KEY_ESC 27
 #define KEY_ENT 10
+#define KEY_TAB 9
 //replace write with printf or other way around? inconsistant 
 
 
@@ -31,6 +36,7 @@ unsigned char * getch() {
 }
 
 void liveRead(unsigned char * line, int count) {
+  int logf = open("fysh.log", O_CREAT | O_WRONLY | O_APPEND, 0644);
   unsigned char * ch;
   int cursorpos = 0;
   int ncp = 0; //new cursorpos
@@ -68,13 +74,42 @@ void liveRead(unsigned char * line, int count) {
           }
         }
       
+    } else if (*ch == KEY_TAB) {
+      char * path = (char *) calloc(1000, sizeof(char));
+      int bp = cursorpos;
+      while(line[bp] != ' ' && bp) {
+        bp--;
+      }
+      if(line[bp] == ' ')
+        bp++;
+
+      strncpy(path, line + bp, cursorpos - bp);
+      //Open cwd
+      DIR * d;
+      d = opendir(".");
+      struct dirent *entry;
+      char ** dirnames = calloc(5000, sizeof(char));
+      int currdir = 0;
+      while(entry = readdir(d)) {
+        if(strncmp(entry->d_name, path, strlen(path))) {
+          dirnames[currdir] = entry->d_name;
+          currdir++;
+        }
+      }
+      if(currdir == 1) {
+        int addcursor = strlen(dirnames[currdir - 1]) - strlen(path);
+        
+        write(logf, dirnames[currdir -1], strlen(dirnames[currdir - 1]) * sizeof(char));
+      
+        //print newline
+        char nl = '\n';
+        write(logf, &nl, sizeof(char));
+      }
     } else if (*ch >= 32 && *ch <= 126) {
       char * tmp = (char *) calloc(1000, sizeof(char));
       strncpy(tmp, line, cursorpos);
       int len = strlen(tmp); 
-      
       strncat(tmp + len, ch, 1);
-      
       len++;
       strcat(tmp + len, line + cursorpos);
       strcpy(line, tmp);
--- io.h
+++ io.h
@@ -1,2 +1,11 @@
-int getch();
-unsigned char * liveRead();
+#ifndef IO
+#define IO
+
+struct histline {
+  int size;
+  char * line;
+};
+
+unsigned char * getch();
+void liveRead();
+#endif
